72151dda1c854b12a737226f0f1582ee3d314d3b
Cartman <vassil.bachvarov@gmail.com>
2019-06-10 12:44:57 +0300

    web link added

--- app/src/main/java/com/example/a05networking/MainActivity.java
+++ app/src/main/java/com/example/a05networking/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.a05networking;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import com.example.a05networking.databinding.ActivityMainBinding;
 
@@ -14,29 +18,81 @@ public class MainActivity extends AppCompatActivity
 {
 
    private ActivityMainBinding binding;
+   private WebView webView;
+   private ProgressBar progess;
+   private EditText webLink;
+   private Button goButton;
+   private String url;
 
    @Override
    protected void onCreate(Bundle savedInstanceState)
    {
       super.onCreate(savedInstanceState);
       binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+      initViews();
+      onClick();
 
-      createWebView();
+   }
+
+   private void onClick() {
+      goButton.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+            url = webLink.getText().toString();
+            createWebView();
+         }
+      });
+   }
+
+   private void initViews() {
+      webView = binding.webViewId;
+      progess = binding.progressBarId;
+      webLink = binding.editTextWebSite;
+      goButton = binding.buttonGo;
    }
 
    private void createWebView()
    {
-      binding.webViewId.setWebViewClient(new WebViewClient()
+      webView.setWebViewClient(new WebViewClient()
       {
          @Override
          public boolean shouldOverrideUrlLoading(WebView view, String url)
          {
-            view.loadUrl(url);
+            loadWebPage(url);
             return false;
          }
+
+         @Override
+         public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            hideProgress();
+         }
+
+
       });
 
-      binding.webViewId.loadUrl("https://www.dnes.bg");
 
+      loadWebPage("https://"+url);
+
+
+   }
+
+   private void hideProgress() {
+      progess.setVisibility(View.GONE);
+   }
+
+   private void loadWebPage(String url) {
+      progess.setVisibility(View.VISIBLE);
+      webView.loadUrl(url);
+   }
+
+
+   @Override
+   public void onBackPressed() {
+      if(webView.canGoBack()){
+         webView.goBack();
+      } else {
+         super.onBackPressed();
+      }
    }
 }
